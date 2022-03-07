734232076fdc7e5b758580e8f6fe738fbae6a281
sergiogval <sergioval@gmail.com>
2022-03-07 16:46:58 -0600

    Add readfile

--- demo_readfile.js
+++ demo_readfile.js
@@ -3,7 +3,7 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
+  fs.readFile('demoFile1.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
