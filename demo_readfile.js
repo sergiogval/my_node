e6bbec55601a8f885c3a56e7b9984914b1e271fa
sergiogval <sergioval@gmail.com>
2022-03-07 11:44:07 -0600

    Add a js file that shows the content of a html file. Only 11 js lines and it seems like you did a whole page

---
+++ demo_readfile.js
@@ -0,0 +1,11 @@
+// The Node.js file system module allows you to work with the file system on your computer.
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080); 
