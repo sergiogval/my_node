786d193b0ac2b2a532b557b0261bc2414a394c39
sergiogval <sergioval@gmail.com>
2022-03-09 08:09:36 -0600

    Add methods unlink and rename

--- myfirst.js
+++ myfirst.js
@@ -7,7 +7,7 @@ var data = 'Hello Nodejs File System';
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write("The date and time are currently: " + dt.myDateTime());
-  res.end('Hello World!');
+  res.end();
 }).listen(8080);
 
 fs.appendFile('mynewfile1.txt', data, function (err) {
@@ -24,4 +24,19 @@ data = 'hello world';
 fs.writeFile('mynewfile3.txt', data, function (err) {
   if (err) throw err;
   console.log('Saved!');
+});
+
+fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
+  if (err) throw err;
+  console.log('Replaced!');
+}); 
+
+fs.unlink('mynewfile2.txt', function (err) {
+  if (err) throw err;
+  console.log('File deleted!');
+});
+
+fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+  if (err) throw err;
+  console.log('File Renamed!');
 });
